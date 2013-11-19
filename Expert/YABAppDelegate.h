78ed62b49f79a99aea79228c182ea999d1f8bda4
Ian Shafer <ian@yabbly.com>
2013-11-19 06:41:36 -0800

    Initial Commit

---
+++ Expert/YABAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YABAppDelegate.h
+//  Expert
+//
+//  Created by Ian Shafer on 11/19/13.
+//  Copyright (c) 2013 Yabbly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YABAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Expert/YABViewController.h
@@ -0,0 +1,13 @@
+//
+//  YABViewController.h
+//  Expert
+//
+//  Created by Ian Shafer on 11/19/13.
+//  Copyright (c) 2013 Yabbly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YABViewController : UIViewController
+
+@end
